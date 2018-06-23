ac764cde031773341ea99635862b581cd56fcf7c
luizpaulofranz <luizpaulofranz@gmail.com>
2018-06-22 22:10:37 -0300

    Adds the state handlers to add and remove ingredients.

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,9 +1,27 @@
+/* this component lists all ingredients controls, using BuildControl child component */
 import React from 'react';
 import BuildControl from './BuildControl/BuildControl';
 
+import classes from './BuildControls.css';
+// this array will be our controls to add/remove ingredients
+const controls = [
+    {label: 'Salad', type: 'salad'},
+    {label: 'Bacon', type: 'bacon'},
+    {label: 'Cheese', type: 'cheese'},
+    {label: 'Meat', type: 'meat'}
+];
+
 const buildControls = (props) => (
-    <div>
-        <BuildControl />
+    <div className={classes.BuildControls}>
+        {controls.map(ctrl => (
+            <BuildControl 
+                key={ctrl.label} 
+                label={ctrl.label} 
+                clickAdd={() => props.ingredientAdded(ctrl.type)}
+                clickRemove={() => props.ingredientRemoved(ctrl.type)}
+                disabled={props.disabled[ctrl.type]}
+            />
+        ))}
     </div>
 );
 
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -6,6 +6,14 @@ import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
 
 import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+
+const INGREDIENT_PRICES = {
+    salad: 0.75,
+    cheese: 1,
+    meat: 2.5,
+    bacon: 1.5
+}
 
 class BurgerBuilder extends Component {
 
@@ -16,16 +24,60 @@ class BurgerBuilder extends Component {
             bacon: 0,
             cheese: 0,
             meat: 0
-        }
+        },
+        // base burger price
+        totalPrice: 5
     }
 
+    addIngredientHandler = (type) =>{
+        const oldCount = this.state.ingredients[type];
+        // the safe way to update state
+        // create a copy of current state part and handle that copy
+        const updatedIngredients = {
+            ...this.state.ingredients
+        };
+        updatedIngredients[type] = (oldCount+1);
+        const ingredientPrice = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const updatedPrice = oldPrice + ingredientPrice;
+        // then finally set the state to our handled state
+        this.setState({totalPrice: updatedPrice, ingredients: updatedIngredients});
+    }
+
+    removeIngredientHandler = (type) =>{
+        const oldCount = this.state.ingredients[type];
+        if (oldCount <= 0 ) {
+            return;
+        }
+        const updatedIngredients = {
+            ...this.state.ingredients
+        };
+        updatedIngredients[type] = (oldCount-1);
+        const ingredientPrice = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const updatedPrice = oldPrice - ingredientPrice;
+        this.setState({totalPrice: updatedPrice, ingredients: updatedIngredients});
+    }
+
+    // and here we pass down our state handlers as props
     render() {
+        // copy disableinfo array
+        const disabledInfo = {
+            ...this.state.ingredients
+        }
+        for (let key in disabledInfo) {
+            // a simple array which contains true or false in each position
+            // used to disable or not the Less button
+            disabledInfo[key] = disabledInfo[key] <= 0
+        }
         return (
             <Aux>
-                <div>
-                    <Burger ingredients={this.state.ingredients}/> 
-                </div>
-                <div>Burger Controls</div>
+                <Burger ingredients={this.state.ingredients}/> 
+                <BuildControls 
+                    ingredientAdded={this.addIngredientHandler} 
+                    ingredientRemoved={this.removeIngredientHandler} 
+                    disabled={disabledInfo}
+                />
             </Aux>
         );
     }
