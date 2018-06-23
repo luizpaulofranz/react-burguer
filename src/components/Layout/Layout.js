e812b7fdde10994e5a3db273763c7e9a7ea93e34
luizpaulofranz <luizpaulofranz@gmail.com>
2018-06-23 11:23:00 -0300

    Adds Modal and backdrop components

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -6,7 +6,7 @@ import classes from './Layout.css';
 
 const layout = ( props ) => (
     <Aux>
-        <div>Toolbar, SideDrawer, Backdrop</div>
+        {/*<div>Toolbar, SideDrawer, Backdrop</div>*/}
         <main className={classes.Content}>
             {props.children}
         </main>
