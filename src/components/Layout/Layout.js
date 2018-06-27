f5be2105fc904c6f1a976ae53c06ff35d172ab10
luizpaulofranz <luizpaulofranz@gmail.com>
2018-06-27 10:51:06 -0300

    Creates the SideDrawer component, and adds it to Layout.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -4,10 +4,12 @@ import Aux from '../../hoc/Aux';
 // configurados com CSS components
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const layout = ( props ) => (
     <Aux>
         <Toolbar />
+        <SideDrawer />
         <main className={classes.Content}>
             {props.children}
         </main>
