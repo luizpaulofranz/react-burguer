9936060fbb2fb3c6033f4caaa2c0f5a3d4290524
luizpaulofranz <luizpaulofranz@gmail.com>
2018-07-02 09:22:21 -0300

    Implements shouldComponentUpdate to controle OrderSummary Renderization.

--- src/components/Ui/Modal/Modal.js
+++ src/components/Ui/Modal/Modal.js
@@ -1,22 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../../hoc/Aux';
 import Backdrop from '../Backdrop/Backdrop';
 
 import classes from './Modal.css';
 
-const modal = (props) => (
-    <Aux>
-        <Backdrop show={props.show} click={props.modalClose} />
-        <div 
-            className={classes.Modal}
-            style={{
-                transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                opacity: props.show ? '1' : '0'
-            }}
-        >
-            {props.children}
-        </div>
-    </Aux>
-);
+class Modal extends Component {
 
-export default modal;
+    shouldComponentUpdate( nextProps, nextState ) {
+        return nextProps.show !== this.props.show;
+    }
+
+    componentWillUpdate() {
+        console.log('[Modal] WillUpdate');
+    }
+
+    render () {
+        return (
+        <Aux>
+            <Backdrop show={this.props.show} click={this.props.modalClose} />
+            <div 
+                className={classes.Modal}
+                style={{
+                    transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                    opacity: this.props.show ? '1' : '0'
+                }}
+            >
+                {this.props.children}
+            </div>
+        </Aux>
+        );
+    }
+}
+
+export default Modal;
