eb88082543d3f02f885fae4a6fcca48d306281ff
luizpaulofranz <luizpaulofranz@gmail.com>
2018-07-10 00:22:58 -0300

    Removing old interceptors from axios on HOC errorHandler.

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -11,16 +11,25 @@ const withErrorHandler = (WrappedComponent, axios) => {
             erro: null
         }
 
+        // here we assign interceptors to axios to handle errors
         componentWillMount () {
-            axios.interceptors.request.use(req => {
+            // we store the interceptors to remove them in componentWillUnmount
+            this.reqInterceptors = axios.interceptors.request.use(req => {
                 // always clean possible previous errors
                 this.setState({error: null});
                 return req;
             });
             // we are interested in intercept only the errors here 
-            axios.interceptors.response.use(res => res, error => {
+            this.resInterceptors = axios.interceptors.response.use(res => res, error => {
                 this.setState({error: error});
             });
+        }
+
+        // and here we remove interceptors to avoid duplications of interceptors
+        // because of multiple instances fos this module
+        componentWillUnmount() {
+            axios.interceptors.request.eject(this.reqInterceptors);
+            axios.interceptors.response.eject(this.resInterceptors);
         }
 
         // to close the modal with error
