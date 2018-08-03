42cb5a33bdf400520c3b1d5abab84e89538fff5f
luizpaulofranz <luizpaulofranz@gmail.com>
2018-08-03 10:50:14 -0300

    Adds Redux and it's very initial settings on project.

---
+++ src/store/reducer.js
@@ -0,0 +1,14 @@
+import * as actionTypes from './actions';
+
+const initialState = {
+    // the ingredients and its quantities
+    ingredients: null,
+    // base burger price 
+    totalPrice: 5
+}
+
+const reducer = (state = initialState, action) => {
+
+};
+
+export default reducer;
