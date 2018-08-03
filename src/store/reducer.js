7054a03b7bb44d7c9908acb8147627b889214f59
luizpaulofranz <luizpaulofranz@gmail.com>
2018-08-03 11:10:17 -0300

    Adds reducer functions to update state.

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -2,13 +2,40 @@ import * as actionTypes from './actions';
 
 const initialState = {
     // the ingredients and its quantities
-    ingredients: null,
+    ingredients: {
+        salad: 0,
+        bacon: 0,
+        cheese: 0,
+        meat: 0
+    },
     // base burger price 
     totalPrice: 5
 }
 
+// always returns a new object, so we never mutate data
 const reducer = (state = initialState, action) => {
-
+    switch (action.type) {
+        case actionTypes.ADD_INGREDIENT:
+            return {
+                ...state,
+                ingredients: {
+                    ...state.ingredients,
+                    // here we override the passed ingredient
+                    // these brakets mean "override this property"
+                    [action.ingredientName]: state[action.ingredientName] +1
+                }
+            };
+        case actionTypes.ADD_INGREDIENT:
+        return {
+            ...state,
+            ingredients: {
+                ...state.ingredients,
+                [action.ingredientName]: state[action.ingredientName] -1
+            }
+        };
+        default:
+            return state;
+    }
 };
 
 export default reducer;
