e17bcc19534997cf0a9581e42b3603b3f01a67cb
luizpaulofranz <luizpaulofranz@gmail.com>
2018-08-22 00:36:38 -0300

    Adds react-thunk to project, and REDUX DEVTOOLS Debug support.

--- src/index.js
+++ src/index.js
@@ -2,14 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+// we need these other imports to add middlewares on our redux flow
+// compose allows us to combine multiple middlewares to pass into store
+// we must compose this becouse store receives only one object
+import { createStore, applyMiddleware, compose } from 'redux';
 
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import reducer from './store/reducer';
+import burgerBuilderReducer from './store/reducers/burgerBuilder';
+import thunk from 'redux-thunk';
+
+// to debug purposes we apply this middleware
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 // creating our redux global state object
-const store = createStore(reducer);
+const store = createStore(
+    burgerBuilderReducer
+    // the second argument is just on object, tha's why we need to compose them
+    ,composeEnhancers(
+        applyMiddleware(thunk)
+    )
+);
 
 const app = (
     <Provider store={store}>
