0739cd1989223f8eb4c0983fd73e7ed9a204ac91
luizpaulofranz <luizpaulofranz@gmail.com>
2018-09-15 21:15:49 -0300

    Adds Logout action on menu option.

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout'
 import Orders from './containers/Checkout/Orders/Orders';
 import Auth from './containers/Auth/Auth';
+import Logout from './containers/Auth/Logout/Logout';
 
 // todo componente deve retornar algum HTML para ser renderizado
 class App extends Component {
@@ -20,6 +21,7 @@ class App extends Component {
             <Route path="/checkout" component={Checkout} />
             <Route path="/orders" component={Orders} />
             <Route path="/auth" component={Auth} />
+            <Route path="/logout" component={Logout} />
             <Route path="/" exact component={BurgerBuilder} />
           </Switch>
         </Layout>
--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,4 +1,4 @@
 /* This file is just a helper to make the imports easy */
 export { addIngredient, removeIngredient, initIngredients } from './burgerBuilder';
 export { purchaseBurger, purchaseInit, fetchOrders } from './order';
-export { auth } from './auth'
+export { auth, logout } from './auth'
